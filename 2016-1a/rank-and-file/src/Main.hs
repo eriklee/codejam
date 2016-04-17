9fa9a96c8b3dd3e8ff73b5ecd79d9d47f4f155e4
Erik Lee <eriklee113@gmail.com>
2016-04-17 23:26:53 +0100

    rank-and-file: or you can just do it the clever easy way...

--- 2016-1a/rank-and-file/src/Main.hs
+++ 2016-1a/rank-and-file/src/Main.hs
@@ -1,16 +1,12 @@
 module Main where
 
-import Control.Monad (replicateM, join, guard)
-import qualified Data.Vector as V
+import Control.Monad (join, replicateM)
 import Data.List (sort)
-import Control.Parallel.Strategies
 
 main :: IO ()
 main = do
   instances <- readInput
-  --print instances
-  let slns = parMap rseq solvePuzzle instances
-  --print slns
+  let slns = map solvePuzzle instances
   mapM_ printSolution slns
 
 --                   C#  n   arrays
@@ -18,9 +14,6 @@ data Instance = Case Int Int [InpArr] deriving (Show)
 --                   N Ranks Files Missing
 data Solution = Sln Int InpArr deriving (Show)
 type InpArr = [Int]
-
-type Row = [Int]
-type Grid = [Row]
 
 readInput :: IO [Instance]
 readInput = do
@@ -46,66 +39,9 @@ printSolution (Sln cn missing) =
 solvePuzzle :: Instance -> Solution
 solvePuzzle (Case cn n arrs) = Sln cn missing
   where
-    start = initPuzzle n arrs
-    (Step _ ranks files [] True) = head $ finishSteps n start
-    missing = getMissing ranks files
+    start = rle . sort . join $ arrs
+    missing = sort . map fst . filter (odd . snd) $ start
 
-getMissing ranks files = filter (/=0) $ map abs $ zipWith (-) (join $ rotate ranks) (join files)
-
-finishSteps :: Int -> Step -> [Step]
-finishSteps n s = (foldl (.) id (replicate (2*n-1) (join . map takeStep))) $ [s]
-
-data Step = Step { stepN :: Int
-                 , sRanks :: Grid
-                 , sFiles :: Grid
-                 , sRest :: [InpArr]
-                 , sUnknown :: Bool
-              } deriving (Show)
-
-initPuzzle :: Int -> [InpArr] -> Step
-initPuzzle n arrs = Step n [h] [] t False
-  where (h:t) = sort arrs
-
-takeStep :: Step -> [Step]
-takeStep s@(Step _ r f [] True) = if length r == length f then [s] else []
-takeStep   (Step n r f [] False) = takeStep (Step n r f [mkUnknown n] True)
-takeStep (Step n ranks files (next:rest) False) = do
-  s <- [ Step n (ranks ++ [next]) files rest False
-       ,  Step n (ranks ++ [mkUnknown n]) files (next:rest) True
-       , Step n ranks (files ++ [next]) rest False
-       , Step n ranks (files ++ [mkUnknown n]) (next:rest) True]
-  guard $ checkConsistent s
-  guard $ length (sRanks s) <= n
-  guard $ length (sFiles s) <= n
-  return s
-
-takeStep (Step n ranks files (next:rest) True) = do
-  s <- [ Step n (ranks ++ [next]) files rest True
-       , Step n ranks (files ++ [next]) rest True]
-  guard $ checkConsistent s
-  guard $ length (sRanks s) <= n
-  guard $ length (sFiles s) <= n
-  return s
-
-checkConsistent :: Step -> Bool
-checkConsistent (Step _ r f _ _) = checkConsistent' r f
-
-checkConsistent' :: Grid -> Grid -> Bool
-checkConsistent' ranks files = and cells
-  where
-    lf = length files
-    lr = length ranks
-    ranks' = take lf $ rotate ranks
-    files' = map (take lr) files
-    cells = zipWith checkCell (join ranks') (join files')
-
-
--- Turns ranks into files
-rotate :: Grid -> Grid
-rotate ([]:_) = []
-rotate rs = map head rs : rotate (map tail rs)
-
-mkUnknown :: Int -> InpArr
-mkUnknown n = replicate n 0
-
-checkCell x y = x == y || x*y==0
+rle :: Eq a => [a] -> [(a,Int)]
+rle [] = []
+rle (x:xs) = (x, length (takeWhile (==x) (x:xs))) : rle (dropWhile (==x) xs)
